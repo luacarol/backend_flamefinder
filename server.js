12586683f97bd9c84ed693b85e4d2d98760220af
Luana Caroliny <lucarolp.deoli@gmail.com>
2024-11-25 19:46:38 -0300

    atualizando dependências

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ import express from "express";
 const avaliacoesRiscoIncendio = [
     {
         id: 1,
-        urlImagem: "https://cdn.pixabay.com/photo/2020/09/12/16/53/forest-5566040_960_720.jpg",
+        urlImagem: "https://projetocolabora.com.br/wp-content/uploads/2022/08/20220819queimadaapui-cristianbraga-greenpeace.jpg",
         descricao: "Imagem de uma floresta densa com vegetação verde e árvores altas.",
         textoAlternativo: "Floresta densa com árvores altas.",
         grauRiscoIncendio: "alto"
     },
     {
         id: 2,
-        urlImagem: "https://cdn.pixabay.com/photo/2019/08/23/09/37/smoke-4423858_960_720.jpg",
+        urlImagem: "https://img.freepik.com/fotos-premium/caminhoes-de-bombeiros-estao-pegando-fogo-em-um-predio-que-esta-queimando_717440-6523.jpg",
         descricao: "Imagem de uma região urbana com fumaça visível ao fundo.",
         textoAlternativo: "Fumaça visível em área urbana.",
         grauRiscoIncendio: "médio"
     },
     {
         id: 3,
-        urlImagem: "https://cdn.pixabay.com/photo/2020/08/18/15/19/desert-5498673_960_720.jpg",
+        urlImagem: "https://portalimbiara.com.br/dados/noticia/12427/imagem/whatsapp-image-2023-03-20-at-15-40-46.jpeg",
         descricao: "Imagem de um campo aberto com pouca vegetação e terreno árido.",
         textoAlternativo: "Campo aberto com terreno árido e pouca vegetação.",
         grauRiscoIncendio: "baixo"
